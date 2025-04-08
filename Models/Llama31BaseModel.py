30795fcc805b15126614a11d81739b445127b847
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-04-08 23:52:06 +0530

    fix

--- Models/Llama31BaseModel.py
+++ Models/Llama31BaseModel.py
@@ -1,5 +1,5 @@
 from Models.ModelAbstract import ModelAbstract
-from ModelEnum import ModelEnum
+from ..ModelEnum import ModelEnum
 class Llama31BaseModel(ModelAbstract):
     def __init__(self, model_name=ModelEnum.LLAMA_31_8B):
         self.model_name = model_name
