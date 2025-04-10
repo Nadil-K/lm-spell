9c5a3b1e030c15cf972af25e6dbd9f1ef5e59c5a
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-04-10 18:28:23 +0530

    refactor: model enum

--- ModelEnum.py
+++ ModelEnum.py
@@ -1,5 +1,7 @@
 from enum import Enum
+from Models.ModelAbstract import ModelAbstract
 from typing import Dict, Type
+
 
 class ModelEnum(Enum):
     """
@@ -7,32 +9,14 @@ class ModelEnum(Enum):
     Neural Spell Checker Library
     """
     # Fine-tuned models
+    LLAMA31_FT_SI = "surge-masks/llama-3.1-8b-ft-ssc"
+    GEMMA2_FT_SI = "surge-masks/gemma-2-9b-ft-ssc"
     MT5_FT_SI = "surge-masks/mt5-base-ft-ssc"
     MT5_XL_FT_SI = "surge-masks/mt5-xl-ft-ssc"
     MBART_FT_SI = "surge-masks/mbart50-ft-ssc"
     SINBERT_FT_SI = "surge-masks/sinbert-large-ft-ssc"
     XLMR_FT_SI = "surge-masks/xlmr-base-ft-ssc"
-    LLAMA31_FT_SI = "surge-masks/llama-3.1-8b-ft-ssc"
-    GEMMA2_FT_SI = "surge-masks/gemma-2-9b-ft-ssc"
-    
-    # MT5 models
-    MT5_SMALL = "google/mt5-small"
-    MT5_BASE = "google/mt5-base"
-    MT5_LARGE = "google/mt5-large"
-    MT5_XL = "google/mt5-xl"
-    MT5_XXL = "google/mt5-xxl"
-    
-    # MBART models
-    MBART50 = "facebook/mbart-large-50"
-    
-    # SinBERT models
-    SINBERT_LARGE = "NLPC-UOM/SinBERT-large"
-    SINBERT_SMALL = "NLPC-UOM/SinBERT-small"
-    
-    # XLMR models
-    XLMR_BASE = "FacebookAI/xlm-roberta-base"
-    XLMR_LARGE = "FacebookAI/xlm-roberta-large"
-    
+
     # Llama 3.1 models
     LLAMA_31_8B = "meta-llama/Llama-3.1-8B"
     LLAMA_31_8B_INSTRUCT = "meta-llama/Llama-3.1-8B-Instruct"
@@ -49,34 +33,88 @@ class ModelEnum(Enum):
     GEMMA_2_9B = "google/gemma-2-9b"
     GEMMA_2_9B_INSTRUCT = "google/gemma-2-9b-Instruct"
 
-    @staticmethod
-    def getModelClass(modelEnum):
-        from Models.Llama31BaseModel import Llama31BaseModel
-        from Models.Mbart50Model import Mbart50LargeModel
-        from Models.Mt5BaseModel import Mt5BaseModel
-        from Models.SinBertLargeModel import SinBertLargeModel
-        from Models.XlmrBaseModel import XlmrBaseModel
-        # Import other model classes as needed
-
-        modelClasses: Dict[ModelEnum, Type] = {
-            ModelEnum.LLAMA_31_8B: Llama31BaseModel,
-            ModelEnum.MBART50: Mbart50LargeModel,
-            ModelEnum.MT5_BASE: Mt5BaseModel,
-            ModelEnum.XLMR_BASE: XlmrBaseModel,
-            ModelEnum.SINBERT_LARGE: SinBertLargeModel,
-            # Add other model mappings here
-            # You'll need to create appropriate model classes for each new model type
-        }
-        return modelClasses.get(modelEnum)
+    # MT5 models
+    MT5_SMALL = "google/mt5-small"
+    MT5_BASE = "google/mt5-base"
+    MT5_LARGE = "google/mt5-large"
+    MT5_XL = "google/mt5-xl"
+    MT5_XXL = "google/mt5-xxl"
     
-    @staticmethod
-    def get_model_path(name: str):
-        """Get the model path from the model name"""
-        for model in ModelEnum:
-            return model.value
-        raise ValueError(f"Model {name} not found")
+    # MBART models
+    MBART50 = "facebook/mbart-large-50"
+    
+    # SinBERT models
+    SINBERT_SMALL = "NLPC-UOM/SinBERT-small"
+    SINBERT_LARGE = "NLPC-UOM/SinBERT-large"
+    
+    # XLMR models
+    XLMR_BASE = "FacebookAI/xlm-roberta-base"
+    XLMR_LARGE = "FacebookAI/xlm-roberta-large"
+
+    def getModelClass(self) -> ModelAbstract:
+        """Return the model class associated with the enum value"""
+
+        from Models.Llama.LlamaAbstractModel import LlamaAbstractModel
+        from Models.Gemma.GemmaAbstractModel import GemmaAbstractModel
+        from Models.Mt5.Mt5AbstractModel import Mt5AbstractModel
+        from Models.Mbart.MbartAbstractModel import MbartAbstractModel
+        from Models.SinBert.SinBertAbstractModel import SinBertAbstractModel
+        from Models.Xlmr.XlmrAbstractModel import XlmrAbstractModel
+        # import other model classes as needed
+
+        modelClasses: Dict[ModelEnum, ModelAbstract] = {
+            # Fine-tuned models
+            ModelEnum.LLAMA31_FT_SI: LlamaAbstractModel,
+            ModelEnum.GEMMA2_FT_SI: GemmaAbstractModel,
+            ModelEnum.MT5_FT_SI: Mt5AbstractModel,
+            ModelEnum.MT5_XL_FT_SI: Mt5AbstractModel,
+            ModelEnum.MBART_FT_SI: MbartAbstractModel,
+            ModelEnum.SINBERT_FT_SI: SinBertAbstractModel,
+            ModelEnum.XLMR_FT_SI: XlmrAbstractModel,
+
+            # Llama 3.1 models
+            ModelEnum.LLAMA_31_8B: LlamaAbstractModel,
+            ModelEnum.LLAMA_31_8B_INSTRUCT: LlamaAbstractModel,
+
+            # Llama 3.2 models
+            ModelEnum.LLAMA_32_1B: LlamaAbstractModel,
+            ModelEnum.LLAMA_32_1B_INSTRUCT: LlamaAbstractModel,
+            ModelEnum.LLAMA_32_3B: LlamaAbstractModel,
+            ModelEnum.LLAMA_32_3B_INSTRUCT: LlamaAbstractModel,
+
+            # Gemma 2 models
+            ModelEnum.GEMMA_2_2B: GemmaAbstractModel,
+            ModelEnum.GEMMA_2_2B_INSTRUCT: GemmaAbstractModel,
+            ModelEnum.GEMMA_2_9B: GemmaAbstractModel,
+            ModelEnum.GEMMA_2_9B_INSTRUCT: GemmaAbstractModel,
+
+            # mT5 models
+            ModelEnum.MT5_SMALL: Mt5AbstractModel,
+            ModelEnum.MT5_BASE: Mt5AbstractModel,
+            ModelEnum.MT5_LARGE: Mt5AbstractModel,
+            ModelEnum.MT5_XL: Mt5AbstractModel,
+            ModelEnum.MT5_XXL: Mt5AbstractModel,
+
+            # mBART models
+            ModelEnum.MBART50: MbartAbstractModel,
+
+            # SinBERT models
+            ModelEnum.SINBERT_SMALL: SinBertAbstractModel,
+            ModelEnum.SINBERT_LARGE: SinBertAbstractModel,
+            
+            # XLMR models
+            ModelEnum.XLMR_BASE: XlmrAbstractModel,
+            ModelEnum.XLMR_LARGE: XlmrAbstractModel,
+
+            # Add other model-class mappings here
+        }
+        return modelClasses.get(self)
+    
+    def get_model_path(self) -> str:
+        """Return the model path"""
+        return self.value
     
     @staticmethod
     def get_supported_models():
         """Return a list of all supported model names"""
-        return [model.name.lower() for model in ModelEnum]
+        return [model.value for model in ModelEnum]
