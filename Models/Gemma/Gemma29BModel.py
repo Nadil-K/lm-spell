759baea07958d2dc1a498844c34c5b22a57aeeb9
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-04-27 18:08:41 +0530

    gemma

---
+++ Models/Gemma/Gemma29BModel.py
@@ -0,0 +1,39 @@
+from Models.Gemma.GemmaAbstractModel import GemmaAbstractModel
+from ModelEnum import ModelEnum
+
+class Gemma29BModel(GemmaAbstractModel):
+
+    def __init__(self):
+        self.model_label = ModelEnum.GEMMA_2_9B
+
+        self.max_seq_length = 2048
+        self.dtype = None
+        self.load_in_4bit = True
+        self.seed = 42      # get as a parameter
+        self.do_sample = True       # get as a parameter
+
+        super().__init__()
+
+    def correctFromFile(self, src):
+        pass
+    
+    def evaluate(self, src, target):
+        pass
+    
+    def evaluateFromFile(self, src_file, target_file):
+        pass
+    
+    def load_model(self):
+        pass
+    
+    def save_model(self):
+        pass
+    
+    def get_model_name(self):
+        pass
+    
+    def load_tokenizer(self):
+        pass
+    
+    def save_tokenizer(self):
+        pass
