74e638da9741f78825be367e8d92e767947d18ef
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-05-12 02:08:28 +0530

    config refactor

--- tests/Utils/test_config_utils.py
+++ tests/Utils/test_config_utils.py
@@ -6,17 +6,15 @@ def test_get_configs():
 
     mock_config_data = json.dumps({
         "dataset": {
-            "seq2seq": {
-                "inputs": "text",
-                "targets": "expected"
-            }
+            "inputs": "text",
+            "targets": "expected"
         }
     })
     
     with patch("builtins.open", mock_open(read_data=mock_config_data)), patch("os.path.exists", return_value=True):
 
         config = ConfigUtils()
-        assert config.get('dataset.seq2seq.inputs') == 'text'
-        assert config.get('dataset.seq2seq.targets') == 'expected'
-        assert config.get('dataset.seq2seq.non_existing_key') is None
+        assert config.get('dataset.inputs') == 'text'
+        assert config.get('dataset.targets') == 'expected'
+        assert config.get('dataset.non_existing_key') is None
         assert config.get('non_existing_key', 'default_value') == 'default_value'
