7559ab3b5c01de30c41e2b75be61c8d241a859a7
Akesh Samuditha <akeshsamuditha@gmail.com>
2025-05-28 22:14:31 +0530

    wip

--- Trainer/AbstractSeqTrainer.py
+++ Trainer/AbstractSeqTrainer.py
@@ -13,6 +13,89 @@ from Trainer.AbstractTrainer import AbstractTrainer
 from Trainer.Utils.EarlyStopping import EarlyStopping
 from transformers import get_linear_schedule_with_warmup
 
+
+def train(*args):
+    trainer = AbstractSeqTrainer(*args)
+    trainer.train()
+
+class MultiGPUTrainer():
+    def __init__(
+            self,
+            model_instance,
+            train_path,
+            val_path,
+            exp_name,
+            batch_size = 8,
+            epochs = 20,
+            lr = 1e-5,
+            dataset = LMSpellDataset,
+            special_tokens_to_add = None,
+            dataset_size = 1,
+            num_warmup_steps = 1000,
+            train_batch_size = 16,
+            train_max_length = 128,
+            zero_stage = 2,
+            gradient_accumulation_steps = 1,
+            patience = 3,
+            lr_and_opt_path = None,
+            resume_training_from = None,
+            resume_training = False,
+        ):
+        self.model_instance = model_instance
+        self.train_path = train_path
+        self.val_path = val_path
+        self.exp_name = exp_name
+        self.exp_dir = os.path.join(os.getcwd(), self.exp_name)
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.lr = lr
+        self.dataset = dataset
+        self.special_tokens_to_add = special_tokens_to_add
+        self.dataset_size = dataset_size
+        self.num_warmup_steps = num_warmup_steps
+        self.train_batch_size = train_batch_size
+        self.train_max_length = train_max_length
+        self.zero_stage = zero_stage
+        self.gradient_accumulation_steps = gradient_accumulation_steps
+        self.lr_and_opt_path = lr_and_opt_path
+        self.resume_training_from = resume_training_from
+        self.resume_training = resume_training
+        self.train_dataloader = None
+        self.val_dataloader = None
+        self.step_counter = 0
+        self.patience = patience
+    
+    def train(self):
+        from accelerate import notebook_launcher
+        # notebook_launcher(self._train, num_processes=ConfigUtils().get("accelerator.NUM_PROCESSES", 1))
+        notebook_launcher(
+            train, 
+            args = (
+                self.model_instance,
+                self.train_path,
+                self.val_path,
+                self.exp_name,
+                self.batch_size,
+                self.epochs,
+                self.lr,
+                self.dataset,
+                self.special_tokens_to_add,
+                self.dataset_size,
+                self.num_warmup_steps,
+                self.train_batch_size,
+                self.train_max_length,
+                self.zero_stage,
+                self.gradient_accumulation_steps,
+                self.patience,
+                self.lr_and_opt_path,
+                self.resume_training_from,
+                self.resume_training
+                ), 
+            num_processes=2, 
+            mixed_precision = 'fp16')    
+    
+
+
 class AbstractSeqTrainer(AbstractTrainer):
 
     def __init__(
@@ -61,7 +144,7 @@ class AbstractSeqTrainer(AbstractTrainer):
         self.step_counter = 0
         self.patience = patience
 
-    def _train(self):
+    def train(self):
         from accelerate import Accelerator, DeepSpeedPlugin
         GeneralUtils.clean_memory()
         deepspeed_plugin = DeepSpeedPlugin(
@@ -105,13 +188,6 @@ class AbstractSeqTrainer(AbstractTrainer):
             self.accelerator.wait_for_everyone() 
             if self.accelerator.check_trigger():
                     break
-
-    def train(self, *kwrgs):
-        a = 2
-        b = 3
-        from accelerate import notebook_launcher
-        # notebook_launcher(self._train, num_processes=ConfigUtils().get("accelerator.NUM_PROCESSES", 1))
-        notebook_launcher(self._train, args = (a, b, ), num_processes=2, mixed_precision = 'fp16', log_line_prefix_template = "Test")
         
     def validate(self):
         self.accelerator.print("Validation Started")
