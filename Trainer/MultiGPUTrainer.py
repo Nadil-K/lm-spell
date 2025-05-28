eb1aaf07a011e4e1ced9f222eb74795fe33ce30c
Akesh Samuditha <akeshsamuditha@gmail.com>
2025-05-28 23:02:01 +0530

    wip

--- Trainer/MultiGPUTrainer.py
+++ Trainer/MultiGPUTrainer.py
@@ -1,12 +1,12 @@
 
 import os
 from Data.LMSpellDataset import LMSpellDataset
-import multiprocessing as mp
-try:
-    mp.set_start_method("spawn", force=True)
-except RuntimeError:
-    print("Spawn method already set, continuing...") 
-    pass
+# import multiprocessing as mp
+# try:
+#     mp.set_start_method("spawn", force=True)
+# except RuntimeError:
+#     print("Spawn method already set, continuing...") 
+#     pass
 
 def _train(*args):
     from Trainer.Seq2SeqTrainer import Seq2SeqTrainer
