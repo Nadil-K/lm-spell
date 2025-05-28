d4b25d6dd774b1b3d55b0e5bfbad8bb362c26ac5
Akesh Samuditha <akeshsamuditha@gmail.com>
2025-05-29 01:20:09 +0530

    test

--- Models/Mt5/Mt5AbstractModel.py
+++ Models/Mt5/Mt5AbstractModel.py
@@ -7,8 +7,10 @@ class Mt5AbstractModel(EncoderDecoderAbstract):
         super().__init__()
         model_path = self.model_label.get_model_path()
         # self.model = MT5ForConditionalGeneration.from_pretrained(model_path).to(self.device)
-        self.model = MT5ForConditionalGeneration.from_pretrained(model_path)
-        self.tokenizer = T5TokenizerFast.from_pretrained(model_path)
+        # self.model = MT5ForConditionalGeneration.from_pretrained(model_path)
+        # self.tokenizer = T5TokenizerFast.from_pretrained(model_path)
+        self.model = None
+        self.tokenizer = None
     
     def load_model(self):
         pass
